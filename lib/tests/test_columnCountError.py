35f611ba2467b1e67da183258543b8eecb75d752
jbbuisson <jb.buisson@gmail.com>
2021-06-25 00:00:11 -0400

    tests: beginning

---
+++ lib/tests/test_columnCountError.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from lib.columnCountError import ColumnCountError
+
+
+class Test_columnCountError:
+    def test_init(self):
+
+        file = Path('./myFile.csv')
+        exception = ColumnCountError(
+            file,
+            123,
+            111,
+            10)
+
+        expected_message = 'myFile.csv - line 123 - Expected 111 column(s), but got 10'
+        assert(exception.message == expected_message)
